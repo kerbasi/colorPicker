45c44bf31a5ae8aa8f1e6b866129bf5e95ab696a
Kirill Novikov <kirill.novikov@gmail.com>
2022-10-21 16:49:59 +0300

    Add random color function

--- src/script.js
+++ src/script.js
@@ -1,1 +1,20 @@
-﻿
+﻿const cols = document.querySelectorAll(".col")
+
+function setColor() {
+    cols.forEach(col => {
+        const hexColor = getRandomColor()
+        col.style.backgroundColor = hexColor
+        col.querySelector("h2").textContent = hexColor
+    })
+}
+
+function getRandomColor(){
+    const numbers = "0123456789ABCDEF"
+    let hexColor = ""
+    for(let i = 0; i < 6; i++){
+        hexColor += numbers[Math.floor(Math.random() * numbers.length)]
+    }
+    return "#" + hexColor
+}
+
+setColor()
